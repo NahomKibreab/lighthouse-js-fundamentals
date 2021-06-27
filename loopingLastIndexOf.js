c2880febba8531af9d5c67e6e6b901b21ba795a1
Nahom Mehanzel <nknahom@gmail.com>
2021-06-27 19:33:37 +0000

    Looping Last IndexOf Challenge

---
+++ loopingLastIndexOf.js
@@ -0,0 +1,15 @@
+const lastIndexOf = function(array,value){
+  let lastValue = -1;
+  array.forEach((element , index) => {
+    if(value === element){
+      lastValue = index;
+    }
+  });
+  return lastValue;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
