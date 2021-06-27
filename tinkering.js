dd4c4ff5295aa97f9b89408218cb0e70bd6384ad
Nahom Mehanzel <nknahom@gmail.com>
2021-06-27 22:39:16 +0000

    Objects - We're Rooting For You! Challenge

--- tinkering.js
+++ tinkering.js
@@ -1,9 +1,15 @@
-const carPassing = function (cars, speed) {
+const judgeVegetable = function (vegetables, metric) {
   // Your code in here ...
-  let newObj = {
-    time: Date.now(),
-    speed: speed
-  };
-  cars.push(newObj);
-  return cars;
-}
+  let largest = 0;
+  let name = '';
+  for(let vegetable of vegetables){
+    if(vegetable[metric] > largest){
+      largest = vegetable[metric];
+      name = vegetable.submitter;
+    }
+  }
+  console.log(name);
+  return name;
+};
+
+
